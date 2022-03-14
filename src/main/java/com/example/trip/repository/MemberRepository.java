603a64b918df2b9d397539a9b23f1998813e8b05
alisyabob <sjs939883@gmail.com>
2022-03-14 21:32:55 +0900

    [FIX] 중복제거를 위한 fetch join distinct 추가

--- src/main/java/com/example/trip/repository/MemberRepository.java
+++ src/main/java/com/example/trip/repository/MemberRepository.java
@@ -13,7 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.plan.id =:plan_id and m.user.id =:user_id")
     Optional<Member> findByNickNameAndPlanId(@Param("user_id") Long user_id,@Param("plan_id") Long plan_id);
 
-    @Query("select m from Member m left join fetch m.user left join fetch m.plan where m.plan.id=:planId ")
+    @Query("select distinct m from Member m left join fetch m.user left join fetch m.plan where m.plan.id=:planId ")
     List<Member> findPlanAndMembers(@Param("planId") Long planId);
 
     @Modifying
