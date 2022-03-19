16e5b8ac6326e72b61f418e03e1297083a8996e8
김윤민 <dbsalszz@naver.com>
2022-03-19 23:41:20 +0900

    [FIX] Feed 엔티티 @NotBlank 어노테이션 추가, 나머지 필드들도 빈 문자열 체크

--- src/main/java/com/example/trip/dto/request/FeedRequestDto.java
+++ src/main/java/com/example/trip/dto/request/FeedRequestDto.java
@@ -14,43 +14,53 @@ import java.util.List;
 public class FeedRequestDto {
 
     @Getter
-        public static class FeedRequestRegisterDto{
-            @NotBlank
-            private String title;
-
-//        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-        private LocalDateTime travelStart;
-
-//        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-        private LocalDateTime travelEnd;
-
-        private List<FeedDetail> feedDetail;
-    }
-
-    @Getter
-    public static class FeedRequestModifyDto{
+    public static class FeedRequestRegisterDto {
+        @NotBlank
         private String title;
 
+        //        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @NotBlank
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime travelStart;
 
+        //        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @NotBlank
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         private LocalDateTime travelEnd;
 
         private List<FeedDetail> feedDetail;
     }
 
     @Getter
-    public static class FeedRequestCommentRegisterDto{
+    public static class FeedRequestModifyDto {
+        @NotBlank
+        private String title;
+
+        @NotBlank
+        private LocalDateTime travelStart;
+
+        @NotBlank
+        private LocalDateTime travelEnd;
+
+        @NotBlank
+        private List<FeedDetail> feedDetail;
+    }
+
+    @Getter
+    public static class FeedRequestCommentRegisterDto {
+        @NotBlank
         private String content;
     }
 
     @Getter
-    public static class FeedRequestCommentModifyDto{
+    public static class FeedRequestCommentModifyDto {
+        @NotBlank
         private String content;
     }
+
     @Getter
-    public static class FeedRequestDeleteImgDto{
+    public static class FeedRequestDeleteImgDto {
+        @NotBlank
         private List<String> fileNames;
     }
 }
