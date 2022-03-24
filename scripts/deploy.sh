124df533b5a62347eca54b6919e979f6a9760287
alisyabob <sjs939883@gmail.com>
2022-03-24 20:22:30 +0900

    [FIX] jar파일 경로 및 project_name 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step2/zip/build/libs
-PROJECT_NAME=BE
+REPOSITORY=/home/ec2-user/app/step2/zip
+PROJECT_NAME=trip-0.0.1-SNAPSHOT
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
