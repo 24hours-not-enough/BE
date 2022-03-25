1bea7084ae7c7dbd02048d46aa1aa901cffb8c39
김윤민 <dbsalszz@naver.com>
2022-03-25 10:22:26 +0900

    [FIX] socialLoginServiceImpl 위치 옮긴 후 푸시

--- src/main/java/com/example/trip/service/impl/SocialLoginServiceImpl.java
+++ src/main/java/com/example/trip/service/impl/SocialLoginServiceImpl.java
@@ -94,7 +94,7 @@ public class SocialLoginServiceImpl implements SocialLoginService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", kakao_client_id);
-        body.add("redirect_uri", "http://localhost:8080/api/kakaologin");
+        body.add("redirect_uri", "http://13.209.47.53/api/kakaologin");
         body.add("code", code);
         body.add("client_secret", kakao_client_secret);
 
