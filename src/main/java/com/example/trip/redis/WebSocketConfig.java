c4300bfe598f3d0021887743278983f3650f2bc3
Shinnybest <kim295521@naver.com>
2022-03-30 00:25:04 +0900

    [FIX] Compile Error 수정

--- src/main/java/com/example/trip/redis/WebSocketConfig.java
+++ src/main/java/com/example/trip/redis/WebSocketConfig.java
@@ -3,13 +3,12 @@ package com.example.trip.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@RequiredArgsConstructor
+
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
