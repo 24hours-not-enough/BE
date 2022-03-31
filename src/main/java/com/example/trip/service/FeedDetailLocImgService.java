a85fa259736ea88b67f6093bbc9902fe5859a874
김윤민 <dbsalszz@naver.com>
2022-04-01 00:58:46 +0900

    [FIX] FeedDetailLocImgServiceImpl 분리, 주석 추가

--- src/main/java/com/example/trip/service/FeedDetailLocImgService.java
+++ src/main/java/com/example/trip/service/FeedDetailLocImgService.java
@@ -1,41 +1,12 @@
 package com.example.trip.service;
 
-import com.example.trip.advice.exception.FeedDetailLocNotFoundException;
-import com.example.trip.domain.FeedDetailLoc;
-import com.example.trip.domain.FeedDetailLocImg;
 import com.example.trip.dto.request.FeedRequestDto;
-import com.example.trip.repository.FeedDetailLocImgRepository;
-import com.example.trip.repository.FeedDetailLocRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
 
-@RequiredArgsConstructor
-@Service
-public class FeedDetailLocImgService {
-    private final FeedDetailLocRepository feedDetailLocRepository;
-    private final FeedDetailLocImgRepository feedDetailLocImgRepository;
-    private final AwsS3Service awsS3Service;
-
-    public Map<String, String> registerFeedImage(Long feedDetailLocId, List<MultipartFile> imgFiles) {
-        Map<String, String> nameAndUrl = awsS3Service.uploadFile(imgFiles);
-        // 해당 피드 상세 위치 값이 있는지 체크
-        FeedDetailLoc feedDetailLoc = feedDetailLocRepository.findById(feedDetailLocId)
-                .orElseThrow(() -> new FeedDetailLocNotFoundException());
-        nameAndUrl.entrySet().forEach(x -> feedDetailLocImgRepository.save(FeedDetailLocImg.builder()
-                .feedDetailLoc(feedDetailLoc)
-                .fileName(x.getKey())
-                .imgUrl(x.getValue())
-                .build()));
-
-        return nameAndUrl;
-    }
-
-    public void deleteFeedImage(FeedRequestDto.FeedRequestDeleteImgDto FeedRequestDeleteImgDto) {
-        List<String> fileNames = FeedRequestDeleteImgDto.getFileNames();
-        fileNames.forEach(x -> awsS3Service.deleteFile(x));
-    }
+public interface FeedDetailLocImgService {
+    Map<String, String> registerFeedImage(Long feedDetailLocId, List<MultipartFile> imgFiles);
+    void deleteFeedImage(FeedRequestDto.FeedRequestDeleteImgDto FeedRequestDeleteImgDto);
 }
--- src/main/java/com/example/trip/service/impl/FeedDetailLocImgServiceImpl.java
+++ src/main/java/com/example/trip/service/impl/FeedDetailLocImgServiceImpl.java
@@ -1,4 +1,52 @@
 package com.example.trip.service.impl;
 
-public class FeedDetailLocImgServiceImpl {
+import com.example.trip.advice.exception.FeedDetailLocNotFoundException;
+import com.example.trip.domain.FeedDetailLoc;
+import com.example.trip.domain.FeedDetailLocImg;
+import com.example.trip.dto.request.FeedRequestDto;
+import com.example.trip.repository.FeedDetailLocImgRepository;
+import com.example.trip.repository.FeedDetailLocRepository;
+import com.example.trip.service.AwsS3Service;
+import com.example.trip.service.FeedDetailLocImgService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class FeedDetailLocImgServiceImpl implements FeedDetailLocImgService {
+
+    private final FeedDetailLocRepository feedDetailLocRepository;
+    private final FeedDetailLocImgRepository feedDetailLocImgRepository;
+    private final AwsS3Service awsS3Service;
+
+    @Override
+    @Transactional
+    public Map<String, String> registerFeedImage(Long feedDetailLocId, List<MultipartFile> imgFiles) {
+        //s3에 이미지 파일 업로드
+        Map<String, String> nameAndUrl = awsS3Service.uploadFile(imgFiles);
+        // 해당 피드 상세 위치 값이 있는지 체크
+        FeedDetailLoc feedDetailLoc = feedDetailLocRepository.findById(feedDetailLocId)
+                .orElseThrow(() -> new FeedDetailLocNotFoundException());
+        //FeedDetailLocImg 에 관련 정보 담아서 저장
+        nameAndUrl.entrySet().forEach(x -> feedDetailLocImgRepository.save(FeedDetailLocImg.builder()
+                .feedDetailLoc(feedDetailLoc)
+                .fileName(x.getKey())
+                .imgUrl(x.getValue())
+                .build()));
+
+        return nameAndUrl;
+    }
+
+    @Override
+    @Transactional
+    public void deleteFeedImage(FeedRequestDto.FeedRequestDeleteImgDto FeedRequestDeleteImgDto) {
+        List<String> fileNames = FeedRequestDeleteImgDto.getFileNames();
+        // 파일 이름으로 삭제
+        fileNames.forEach(x -> awsS3Service.deleteFile(x));
+    }
 }
