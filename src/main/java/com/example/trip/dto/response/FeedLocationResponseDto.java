e0c41503dcc1fb8332b0d929d67e60814f1175d2
김윤민 <dbsalszz@naver.com>
2022-04-01 17:51:28 +0900

    [FIX] 메인페이지 API : api/map 수정. - 기존의 모든 피드를 가지고 오던 것에서 위도와 경도 범위에 따라 가지고 오게 수정. - 이에따른 Dto 추가 - 쿼리수정

--- src/main/java/com/example/trip/dto/response/FeedLocationResponseDto.java
+++ src/main/java/com/example/trip/dto/response/FeedLocationResponseDto.java
@@ -1,11 +1,8 @@
 package com.example.trip.dto.response;
 
 import com.example.trip.domain.FeedLocation;
-import com.example.trip.dto.response.FeedDetailLocResponseDto;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,8 +10,7 @@ import java.util.stream.Collectors;
 public class FeedLocationResponseDto {
 
     @AllArgsConstructor
-    @Data
-    @NoArgsConstructor
+    @Getter
     public static class BookMark {
         private Long placeId;
         private String placeName;
@@ -28,9 +24,10 @@ public class FeedLocationResponseDto {
             this.placeId = feedLocation.getId();
             this.placeName = feedLocation.getName();
             this.placeAddress = feedLocation.getPlaceAddress();
-            this.latitude = feedLocation.getLatitude();
-            this.longitude = feedLocation.getLongitude();
+            this.latitude = feedLocation.getLatitude().toString();
+            this.longitude = feedLocation.getLongitude().toString();
             this.feedDetailLoc = feedLocation.getFeedDetailLocs().stream().map(FeedDetailLocResponseDto.GetFeedDetailLoc::new).collect(Collectors.toList());
         }
     }
 }
+
