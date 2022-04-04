0552885dd15ca2364e9bd018db6f1d93f857c097
김윤민 <dbsalszz@naver.com>
2022-04-04 15:13:56 +0900

    [FEAT] Controller단 주석처리 : BoomMarkController, FeedCommentController, FeedController, FeedDetailLocImgController, HomeController, LikesController

--- src/main/java/com/example/trip/controller/LikesController.java
+++ src/main/java/com/example/trip/controller/LikesController.java
@@ -23,7 +23,9 @@ public class LikesController {
     @PostMapping("/feed/{feedDetailLocId}/like")
     public ResponseEntity<FeedResponseDto.FeedResponseDefault> likeFeed(@PathVariable Long feedDetailLocId,
                                                                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 좋아요를 등록하는 함수 호출
         likesService.likeFeed(feedDetailLocId, userDetails.getUser());
+        //약속한 리스펀스 엔티티 형태로 만들어서 리턴
         return new ResponseEntity<>(FeedResponseDto.FeedResponseDefault.builder()
                 .result("success")
                 .msg("피드 좋아요 등록에 성공하였습니다.")
@@ -32,7 +34,9 @@ public class LikesController {
 
     @DeleteMapping("/feed/{feedDetailLocId}/unlike")
     public ResponseEntity<FeedResponseDto.FeedResponseDefault> unlikeFeed(@PathVariable Long feedDetailLocId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        //좋아요를 취소하는 함수 호출
         likesService.unlikeFeed(feedDetailLocId, userDetails.getUser());
+        //약속한 리스펀스 엔티티 형태로 만들어서 리턴
         return new ResponseEntity<>(FeedResponseDto.FeedResponseDefault.builder()
                 .result("success")
                 .msg("피드 좋아요 취소에 성공하였습니다.")
