13770e46cfc05d62eb23ce08ca9a3494fc7cb6af
김윤민 <dbsalszz@naver.com>
2022-04-05 02:00:51 +0900

    [FIX] S3 Upload 역할을 하던 클래스가 중복되어 하나로 합침

--- src/main/java/com/example/trip/controller/MypageController.java
+++ src/main/java/com/example/trip/controller/MypageController.java
@@ -84,8 +84,8 @@ public class MypageController {
     @PutMapping("/api/mypage")
     public ResponseEntity<MypageResponseDto.Response> changeProfile(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                     @RequestPart String username,
-                                                                    @RequestPart MultipartFile file) throws IOException {
-        UserResponseDto.UserProfile info = mypageService.changeProfile(userDetails, username, file);
+                                                                    @RequestPart MultipartFile imgFiles) throws IOException {
+        UserResponseDto.UserProfile info = mypageService.changeProfile(userDetails, username, imgFiles);
         return new ResponseEntity<>(MypageResponseDto.Response.builder()
                                                         .result("success")
                                                         .msg("마이페이지 수정 완료입니다.")
--- src/main/java/com/example/trip/service/impl/FeedDetailLocImgServiceImpl.java
+++ src/main/java/com/example/trip/service/impl/FeedDetailLocImgServiceImpl.java
@@ -1,17 +1,10 @@
 package com.example.trip.service.impl;
 
-import com.example.trip.advice.exception.FeedDetailLocNotFoundException;
-import com.example.trip.domain.FeedDetailLoc;
-import com.example.trip.domain.FeedDetailLocImg;
 import com.example.trip.dto.request.FeedRequestDto;
-import com.example.trip.repository.FeedDetailLocImgRepository;
-import com.example.trip.repository.FeedDetailLocRepository;
-import com.example.trip.service.AwsS3Service;
 import com.example.trip.service.FeedDetailLocImgService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
@@ -19,27 +12,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Service
 public class FeedDetailLocImgServiceImpl implements FeedDetailLocImgService {
-
-    private final FeedDetailLocRepository feedDetailLocRepository;
-    private final FeedDetailLocImgRepository feedDetailLocImgRepository;
-    private final AwsS3Service awsS3Service;
+    private final S3UploaderServiceImpl s3UploaderService;
 
     @Override
     @Transactional
     public Map<String, String> registerFeedImage(List<MultipartFile> imgFiles) {
         //s3에 이미지 파일 업로드
-        Map<String, String> nameAndUrl = awsS3Service.uploadFile(imgFiles);
-//        // 해당 피드 상세 위치 값이 있는지 체크
-//        FeedDetailLoc feedDetailLoc = feedDetailLocRepository.findById(feedDetailLocId)
-//                .orElseThrow(() -> new FeedDetailLocNotFoundException());
-//        //FeedDetailLocImg 에 관련 정보 담아서 저장
-//        nameAndUrl.entrySet().forEach(x -> feedDetailLocImgRepository.save(FeedDetailLocImg.builder()
-//                .feedDetailLoc(feedDetailLoc)
-//                .fileName(x.getKey())
-//                .imgUrl(x.getValue())
-//                .build()));
-
-        return nameAndUrl;
+        return s3UploaderService.uploadFile(imgFiles);
     }
 
     @Override
@@ -47,6 +26,6 @@ public class FeedDetailLocImgServiceImpl implements FeedDetailLocImgService {
     public void deleteFeedImage(FeedRequestDto.FeedRequestDeleteImgDto FeedRequestDeleteImgDto) {
         List<String> fileNames = FeedRequestDeleteImgDto.getFileNames();
         // 파일 이름으로 삭제
-        fileNames.forEach(x -> awsS3Service.deleteFile(x));
+        fileNames.forEach(x -> s3UploaderService.deleteFile(x));
     }
 }
